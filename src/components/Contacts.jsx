9ef584128caee8972c750f1794f43a1497d3e485
IvanNapsterKE <nguyoivan@gmail.com>
2023-07-20 19:35:16 +0300

    updated  contact form

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 
 function Contacts() {
   return (
-    <div>
-      <p>contacts</p>
+   
+  <div className="flex flex-row  items-center">
+
+  <div className="bg-[#d9d9d9] flex flex-col ml-8 gap-5 items-start pt-10 pb-20 px-16">
+    <div className="whitespace-nowrap text-5xl font-['Poppins'] font-bold mb-10 w-[297px]">
+      Get in touch
     </div>
+    <div className="text-3xl font-['Poppins'] ml-3 w-24">Name</div>
+    <div className="bg-white self-stretch h-20 shrink-0 mb-5 mx-3" />
+    <div className="text-3xl font-['Poppins'] ml-3 w-20">Email</div>
+    <div className="bg-white self-stretch h-20 shrink-0 mb-5 mx-3" />
+    <div className="text-3xl font-['Poppins'] ml-3 w-24">Phone</div>
+    <div className="bg-white self-stretch h-20 shrink-0 mb-5 mx-3" />
+    <div className="whitespace-nowrap text-3xl font-['Poppins'] ml-3 w-2/5">
+      How would you like to partner/sponsour us?
+    </div>
+    <div className="bg-white self-stretch h-[408px] shrink-0 mb-8 mx-3" />
+    <div className="bg-[#111a6d] flex flex-col justify-center ml-3 h-[108px] shrink-0 items-center">
+      <div className="text-3xl font-['Poppins'] text-white w-[114px] ml-[108px] mr-[110px]">
+        Submit
+      </div>
+    </div>
+    </div>
+    </div>
+    
+
+
+   
   )
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+// import Facebook from "../assets/facebook.svg";
+// import Twitter from "../assets/twitter.png";
+// import Youtube from "../assets/youtube.svg";
+// import Github from "../assets/github.svg";
+// import Linkedin from "../assets/linkedin.svg";
+// import Instagram from "../assets/instagram.svg";
+
+
+
+
 
 function Footer() {
   return (
     <div>
-      <p>footer</p>
+      <footer class="bg-[#b4d6df]">
+      {/* <ul class="social-icons flex flex-row items-center mx-24">
+        <li > <a href=""className="bg-[#3b5998]"> < Facebook /> </a>  </li>
+        <li> <a href=""  className="bg-[#55acee]"> < Twitter /> </a> </li>
+        <li > <a href="" className="bg-[#cd201f]"> < Youtube /> </a> </li>
+        <li > <a href="" className="bg-[#00405d]"> < Github /> </a> </li>
+        <li > <a href="" className="bg-[#3f729b]"> < Instagram /> </a> </li>
+        <li  className="bg-[#0077b5]" > <a href="" > < Linkedin /> </a> </li>
+
+
+
+    </ul> */}
+      </footer>
     </div>
-  )
+  );
 }
 
 export default Footer;
