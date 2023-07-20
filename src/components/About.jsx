aebdc3ec5b40917abb4d0ad153d2ff14d1f9139b
IvanNapsterKE <nguyoivan@gmail.com>
2023-07-20 21:56:46 +0300

    meet the team about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { FaFacebook } from "react-icons/fa";
+import blank from "../assets/blankavatar.jpg";
+import background from '../assets/devops.jpg';
 
 function About() {
   return (
     <div>
     
-    <div className="bg-[url()] bg-cover bg-50%_50% bg-blend-normal bg-no-repeat flex flex-col mb-24">
-      <div className="bg-black/79 flex flex-col h-[889px] shrink-0 pt-56 pb-[299px] px-24">
+    <div className="bg-black bg-opacity-75 bg-cover bg-50%_50% bg-no-repeat flex flex-col mb-24" style={{ backgroundImage: `url(${background})` }}>
+      <div className=" flex flex-col h-[889px] shrink-0 pt-56 pb-[299px] px-24">
         <div className="text-center text-5xl font-['Poppins'] font-bold text-white ml-8">
           The Nairobi DevOps Community is led by a group of dedicated individuals
           who are passionate about promoting the growth and success of the DevOps
@@ -96,9 +99,173 @@ function About() {
       Our Team
     </div>
  
-    <div className="whitespace-nowrap text-5xl font-['Poppins'] font-bold self-start w-[313px] mb-6 ml-[179px]">
-      Our Partners
-    </div>
+    <div class="flex items-center justify-center min-h-screen bg-white py-48">
+        <div class="flex flex-col">
+          <div class="flex flex-col mt-8">
+            {/* <!-- Meet the Team --> */}
+            <div class="container max-w-7xl px-4">
+              {/* <!-- Section Header --> */}
+              <div class="flex flex-wrap justify-center text-center mb-24">
+                <div class="w-full lg:w-6/12 px-4">
+                  {/* <!-- Header --> */}
+                  <h1 class="text-gray-900 text-4xl font-bold mb-8">
+                    Meet the Team
+                  </h1>
+
+                  {/* <!-- Description --> */}
+                  <p class="text-gray-700 text-lg font-light">
+                    With over 100 years of combined experience, we've got a
+                    well-seasoned team at the helm.
+                  </p>
+                </div>
+              </div>
+
+              {/* <!-- Team Members --> */}
+              <div class="flex flex-wrap">
+                <div class="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
+                  <div class="flex flex-col">
+                    <a href="#" class="mx-auto">
+                      <img
+                        class="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
+                        src={blank}
+                      />
+                    </a>
+
+                    {/* <!-- Details --> */}
+                    <div class="text-center mt-6">
+                      <h1 class="text-gray-900 text-xl font-bold mb-1">
+                        lorem
+                      </h1>
+
+                      <div class="text-gray-700 font-light mb-2">Founder</div>
+
+                      <div
+                        class="flex items-center justify-center opacity-50 hover:opacity-100
+                                transition-opacity duration-300"
+                      >
+                        <a
+                          href="#"
+                          class="flex rounded-full hover:bg-indigo-50 h-10 w-10"
+                        >
+                          <FaFacebook />
+                        </a>
+
+                        <a
+                          href="#"
+                          class="flex rounded-full hover:bg-blue-50 h-10 w-10"
+                        >
+                          <FaFacebook />
+                        </a>
+
+                        <a
+                          href="#"
+                          class="flex rounded-full hover:bg-orange-50 h-10 w-10"
+                        >
+                          <FaFacebook />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
+                  <div class="flex flex-col">
+                    <a href="#" class="mx-auto">
+                      <img
+                        class="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
+                        src={blank}
+                      />
+                    </a>
+
+                    <div class="text-center mt-6">
+                      <h1 class="text-gray-900 text-xl font-bold mb-1">
+                        Lorem
+                      </h1>
+
+                      <div class="text-gray-700 font-light mb-2">
+                        Specialist
+                      </div>
+
+                      <div
+                        class="flex items-center justify-center opacity-50 hover:opacity-100
+                                transition-opacity duration-300"
+                      >
+                        <FaFacebook />
+
+                        <FaFacebook />
+
+                        <FaFacebook />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
+                  <div class="flex flex-col">
+                    <a href="#" class="mx-auto">
+                      <img
+                        class="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
+                        src={blank}
+                      />
+                    </a>
+
+                    <div class="text-center mt-6">
+                      <h1 class="text-gray-900 text-xl font-bold mb-1">
+                        Lorem
+                      </h1>
+
+                      <div class="text-gray-700 font-light mb-2">
+                        Team Member
+                      </div>
+
+                      <div
+                        class="flex items-center justify-center opacity-50 hover:opacity-100
+                                transition-opacity duration-300"
+                      >
+                        <FaFacebook />
+
+                        <FaFacebook />
+
+                        <FaFacebook />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
+                  <div class="flex flex-col">
+                    <a href="#" class="mx-auto">
+                      <img
+                        class="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
+                        src={blank}
+                      />
+                    </a>
+
+                    <div class="text-center mt-6">
+                      <h1 class="text-gray-900 text-xl font-bold mb-1">
+                        Lorem
+                      </h1>
+
+                      <div class="text-gray-700 font-light mb-2">Lorem</div>
+
+                      <div
+                        class="flex items-center justify-center opacity-50 hover:opacity-100
+                                transition-opacity duration-300"
+                      >
+                        <FaFacebook />
+
+                        <FaFacebook />
+
+                        <FaFacebook />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     
   
 
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import { FaFacebook } from "react-icons/fa";
+import blank from "../assets/blankavatar.jpg";
 
 function Homepage() {
   return (
     <div>
       {/* <div className="overflow-hidden flex flex-col justify-end pt-12 gap-8 w-full"> */}
 
-        {/* </div> */}
+      {/* </div> */}
 
+      
     </div>
-  )
+  );
 }
 
 export default Homepage;
